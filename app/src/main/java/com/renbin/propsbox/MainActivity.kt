4cdac8407c897f821733476209ecbc3590ae164b
renbin <renbin90@foxmail.com>
2021-06-21 14:01:22 +0800

    导入androidx库

--- app/src/main/java/com/renbin/propsbox/MainActivity.kt
+++ app/src/main/java/com/renbin/propsbox/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.renbin.propsbox
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.sunnyWeather).setOnClickListener {
+
+        }
     }
 }
